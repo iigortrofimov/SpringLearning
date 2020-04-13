dbfc5b351d16ac9af4fd7d98217e70586b0d25b1
Igor Trofimov <iigortrofimov@yandex.ru>
2020-04-13 11:27:20 +0300

    Add profile editor

--- twister-spring-boot-app/src/main/java/ru/rogi/letscode/service/UserService.java
+++ twister-spring-boot-app/src/main/java/ru/rogi/letscode/service/UserService.java
@@ -9,8 +9,8 @@ import ru.rogi.letscode.domain.Role;
 import ru.rogi.letscode.domain.User;
 import ru.rogi.letscode.repos.UserRepo;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -39,6 +39,12 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
+        sendActivationMessage(user);
+
+        return true;
+    }
+
+    private void sendActivationMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())){
             String message = String.format(
                     "Hello, %s! \n" +
@@ -47,8 +53,6 @@ public class UserService implements UserDetailsService {
             , user.getUsername(), user.getActivationCode());
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -59,5 +63,45 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(null);
         userRepo.save(user);
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(
+                Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged =(email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged){
+            user.setEmail(email);
+            if (!StringUtils.isEmpty(email)){
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (!StringUtils.isEmpty(password)) user.setPassword(password);
+
+        userRepo.save(user);
+
+        if (isEmailChanged) sendActivationMessage(user);
     }
 }
